42ec3a6f6247417bc559d351a5f2ce75be8ce42a
ofir <ofir@idobet.com>
2017-03-16 17:56:58 +0200

    change random color

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 var colors = ["red", "blue", "yellow", "green", "orange", "black", "cyan", "magenta"]
 
+function rand(max) { 
+    return Math.floor(Math.random() * max)
+}
 function changeColor() {
     var col = document.getElementById("changecolor");
-    col.style.backgroundColor = colors[Math.floor((Math.random()*8)+1)];
+    const color = `rgb(${rand(256)},${rand(256)},${rand(256)})`
+    // col.style.backgroundColor = colors[Math.floor((Math.random()*8)+1)];
+    console.log({color})
+    col.style.backgroundColor =  color
 }
