6380deecd225580be32401262207c76470638633
ofir <ofir@idobet.com>
2017-03-16 18:10:57 +0200

    dd

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var colors = ["red", "blue", "yellow", "green", "orange", "black", "cyan", "magenta"]
-
+// fonction to get random number 
 function rand(max) { 
     return Math.floor(Math.random() * max)
 }
+
+//
 function changeColor() {
     var col = document.getElementById("changecolor");
     const color = `rgb(${rand(256)},${rand(256)},${rand(256)})`
-    // col.style.backgroundColor = colors[Math.floor((Math.random()*8)+1)];
     console.log({color})
     col.style.backgroundColor =  color
 }
